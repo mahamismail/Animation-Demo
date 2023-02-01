4b519588491558bd5835a8540516a8bb556db077
mahamismail <maham.ismail95@gmail.com>
2023-02-01 09:43:44 -0500

    with HTML export files

--- Scripts/Fire.gd
+++ Scripts/Fire.gd
@@ -1,18 +1,7 @@
 extends Area2D
 
-onready var anim1 = "SlashRight";
-onready var anim2 = "SlashLeft";
-onready var anim3 ="SlashDown";
-onready var anim4 = "SlashUp";
-
 func _ready():
 	pass
-	
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Fire_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	if body.name == "Player":
